43ada4346998037e9ef835a2636fcb66ba351ae0
unknown <onyiakamsy74@gmail.com>
2026-03-19 14:27:27 -1000

    chore: baseline DripMaxx Expo project

--- babel.config.js
+++ babel.config.js
@@ -1,11 +1,8 @@
 module.exports = function (api) {
-    api.cache(true);
-    console.log("[babel.config] Babel configuration loaded"); // log
-  
-    return {
-      presets: ["babel-preset-expo"],
-      plugins: ["expo-router/babel"],
-    };
+  api.cache(true);
+  console.log("[babel.config] Babel configuration loaded");
+
+  return {
+    presets: ["babel-preset-expo"],
   };
-  
-  
+};
